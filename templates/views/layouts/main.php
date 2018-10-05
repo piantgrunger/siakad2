88cf048280bb51b748b124504263aaee4111114e
unknown <piant.grunger@gmail.com>
2018-10-06 02:12:03 +0700

    material

--- templates/views/layouts/main.php
+++ templates/views/layouts/main.php
@@ -5,9 +5,14 @@ use yii\helpers\Html;
 /* @var $content string */
 
 
-if (Yii::$app->controller->action->id === 'login') { 
+if (class_exists('ramosisw\CImaterial\web\MaterialAsset')) {
+    ramosisw\CImaterial\web\MaterialAsset::register($this);
+}
+
+
+if (Yii::$app->controller->action->id === 'login') {
 /**
- * Do not use this code in your template. Remove it. 
+ * Do not use this code in your template. Remove it.
  * Instead, use the code  $this->layout = '//main-login'; in your controller.
  */
     echo $this->render(
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,4 @@
-s
+
 <?php
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -33,6 +33,10 @@ if (!Yii::$app->user->isGuest) {
     }
 }
 $this->title = 'SIAKAD';
+
+if (class_exists('ramosisw\CImaterial\web\MaterialAsset')) {
+    ramosisw\CImaterial\web\MaterialAsset::register($this);
+}
 ?>
 <?php $this->beginPage(); ?>
 <!DOCTYPE html>
@@ -48,15 +52,84 @@ $this->title = 'SIAKAD';
 <?php $this->beginBody(); ?>
 
 <div class="wrap">
+    <div class="sidebar" data-color="purple" data-image="../assets/img/sidebar-1.jpg">
+			<!--
+		        Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
+		        Tip 2: you can also add an image using data-image tag
+		    -->
+
+			<div class="logo">
+				<a href="http://www.creative-tim.com" class="simple-text">
+					Creative Tim
+				</a>
+			</div>
+
+	    	<div class="sidebar-wrapper">
+	            <ul class="nav">
+	                <li class="active">
+	                    <a href="dashboard.html">
+	                        <i class="material-icons">dashboard</i>
+	                        <p>Dashboard</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="user.html">
+	                        <i class="material-icons">person</i>
+	                        <p>User Profile</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="table.html">
+	                        <i class="material-icons">content_paste</i>
+	                        <p>Table List</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="typography.html">
+	                        <i class="material-icons">library_books</i>
+	                        <p>Typography</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="icons.html">
+	                        <i class="material-icons">bubble_chart</i>
+	                        <p>Icons</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="maps.html">
+	                        <i class="material-icons">location_on</i>
+	                        <p>Maps</p>
+	                    </a>
+	                </li>
+	                <li>
+	                    <a href="notifications.html">
+	                        <i class="material-icons text-gray">notifications</i>
+	                        <p>Notifications</p>
+	                    </a>
+	                </li>
+					<li class="active-pro">
+	                    <a href="upgrade.html">
+	                        <i class="material-icons">unarchive</i>
+	                        <p>Upgrade to PRO</p>
+	                    </a>
+	                </li>
+	            </ul>
+	    	</div>
+	    </div>
+
+	    <div class="main-panel">
     <?php
     NavBar::begin([
         'brandLabel' => 'SIAKAD',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-default navbar-fixed-top',
+            'class' => 'navbar navbar-transparent navbar-absolute',
         ],
     ]);
-
+    ?>
+		<div class="container-fluid">
+	<?php
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
@@ -68,11 +141,14 @@ $this->title = 'SIAKAD';
             ],
         ],
     ]);
+    ?>
+    </div>
+    <?php
 
     NavBar::end();
     ?>
-
-    <div class="container">
+<div class="content">
+    <div class="container-fluid">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]); ?>
@@ -80,13 +156,9 @@ $this->title = 'SIAKAD';
         <?= $content; ?>
     </div>
 </div>
+</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy;&nbsp;Sistem Informasi Akademik <?= date('Y'); ?></p>
-
-    </div>
-</footer>
+</div>
 
 <?php $this->endBody(); ?>
 </body>
